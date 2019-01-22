030129d2b46e9e0b58444f5dec103f8838ae52ac
Sean Carter <scarter0313@gmail.com>
2019-01-22 04:54:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var musicians = ["John Lennon", "Ringo Starr", "George Harrison", "Paul McCartne
 var instruments = ["Guitar", "Drums", "Lead Guitar", "Bass Guitar"]
 
 function theBeatlesPlay(musicians, instruments) {
-  const arr = []
+  const array = []
 
   for (let i = 0, l = musicians.length; i < l; i++) {
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
-  return arr
+  return array
 }
 
 const facts = [
