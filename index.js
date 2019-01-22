f03013c50512841f3face303b8fa383931cd17b1
Sean Carter <scarter0313@gmail.com>
2019-01-22 05:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ function theBeatlesPlay(musicians, instruments) {
   return arr
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+
 
 function johnLennonFacts(facts) {
   const bigFacts = []
